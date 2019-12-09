e6b4fb0caf0af0f7e33aabf5e2c336804d3f9bab
DaryaSova <54115179+DaryaSova@users.noreply.github.com>
2019-12-10 00:07:15 +0400

    add Server

---
+++ src/Server/Server.java
@@ -0,0 +1,46 @@
+package Server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static Socket clientSocket; //сокет для общения
+    private static ServerSocket server; // серверсокет
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+
+    public static void main(String[] args) {
+        try {
+            try {
+                server = new ServerSocket(4004);
+                System.out.println("Сервер запущен!");
+                clientSocket = server.accept();
+                try {// потоки ввода/вывода.
+
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                    String word = in.readLine(); // ждём пока клиент что-нибудь нам напишет
+                    System.out.println(word);
+                    // не долго думая отвечает клиенту
+                    out.write("Сервер : " + word + "\n");
+                    out.flush();
+
+                } finally {
+                    System.out.println("Сокет закрыт");
+                    clientSocket.close();
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("Сервер закрыт!");
+                server.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+}
